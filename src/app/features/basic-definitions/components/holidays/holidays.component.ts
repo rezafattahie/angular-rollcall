6de8bb62c63b983560998fa7901cccf94332b33b
RezaFattahi <rezafatahy@gmail.com>
2023-03-27 17:51:54 +0330

    Added remove methode for holidays

--- src/app/features/basic-definitions/components/holidays/holidays.component.ts
+++ src/app/features/basic-definitions/components/holidays/holidays.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+
+import { NgbDateStruct, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { IModalData } from 'src/app/features/models/modal-data.interface';
 import { BasicDefinitionsService } from '../../services/basic-definitions.service';
 
 @Component({
@@ -9,11 +12,17 @@ import { BasicDefinitionsService } from '../../services/basic-definitions.servic
 })
 export class HolidaysComponent implements OnInit {
 
+  @ViewChild('modalForm', { static: false }) modalForm?: TemplateRef<HTMLElement>;
+
+  modalData: IModalData = { actionMode: 'Delete' };
+  openedModal: any;
   holidays: string[] = [];
   isLoading: boolean = false;
 
   constructor(
-    private basicDefinitionsService: BasicDefinitionsService
+    private basicDefinitionsService: BasicDefinitionsService,
+    private toast: ToastrService,
+    private modal: NgbModal,
   ) { }
 
   ngOnInit() {
@@ -36,11 +45,15 @@ export class HolidaysComponent implements OnInit {
   }
 
   onAddHoliday(date: NgbDateStruct) {
+    if (!date) {
+      this.toast.error('Select a date!')
+    }
     this.isLoading = true;
     const holiday = { date: this.holidayViewCreator(date) };
     this.basicDefinitionsService.addNewItem(holiday, 'holidays').subscribe({
       next: () => {
         this.holidays.push(this.holidayViewCreator(date));
+        this.toast.success(this.holidayViewCreator(date) + ' added to holidays.')
         this.isLoading = false;
       },
       error: () => {
@@ -50,9 +63,38 @@ export class HolidaysComponent implements OnInit {
     console.log('%cholidays.component.ts line:11 event', 'color: white; background-color: #007acc;', date);
   }
 
+  removeHoliday(holiday: string) {
+    // this.modalData = {
+    //   actionMode: 'Delete',
+    //   title: 'Delete Holiday',
+    //   formFields: [{
+    //     name: '', type: 'text',
+    //     caption: `You are about deleting ${holiday} from holidays. Are you sure?`,
+    //     allowNull: true
+    //   }],
+    // }
+    // this.openedModal = this.modal.open(this.modalForm, { centered: false, size: 'lg' });
+    this.isLoading = true;
+    let foundHoliday: { date: string, id: number } = { date: '', id: 0 };
+    this.basicDefinitionsService.getAll('holidays').subscribe({
+      next: (result) => {
+        foundHoliday = result.find((hd: { date: string, id: number }) => {
+          return hd.date === holiday
+        })
+        this.basicDefinitionsService.delete('holidays', foundHoliday.id).subscribe({
+          next: () => {
+            this.holidays = this.holidays.filter(val => {
+              return val !== holiday
+            })
+          }
+        })
+      }
+    })
+
+  }
+
   holidayViewCreator(date: NgbDateStruct) {
     return `${date.year}/${date.month}/${date.day}`;
-
   }
 
 }
